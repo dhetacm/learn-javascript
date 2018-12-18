46cdc779608e9e2d5ec2e243f3b8af99dac675c4
Dheta Catur Maharani <theresiadhetacm@gmail.com>
2018-12-18 10:38:41 +0700

    Modify index.js comparison number

--- comparison-number/index.js
+++ comparison-number/index.js
@@ -8,6 +8,8 @@ console.log(`Number 2 = ${num2}`);
 console.log(`Number 3 = ${num3}`);
 console.log(`Number 4 = ${num4}`);
 
+console.log("Comparing the numbers with 3");
+
 function testNum(a) {
   if (a > 3) {
     return "Greater Than";
@@ -15,7 +17,7 @@ function testNum(a) {
     return "Less than";
   }
 }
-
+console.log("Comparing the numbers with 3");
 console.log(testNum(`${num1}`));
 console.log(testNum(`${num2}`));
 console.log(testNum(`${num3}`));
