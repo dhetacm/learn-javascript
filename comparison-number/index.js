1076310ebb99877873b575fdb45579059b507b3f
Dheta Catur Maharani <theresiadhetacm@gmail.com>
2018-12-18 10:21:39 +0700

    Add number and if else function

---
+++ comparison-number/index.js
@@ -0,0 +1,22 @@
+var num1 = 5;
+var num2 = 37;
+var num3 = -21;
+var num4 = 0.4;
+
+console.log(`Number 1 = ${num1}`);
+console.log(`Number 2 = ${num2}`);
+console.log(`Number 3 = ${num3}`);
+console.log(`Number 4 = ${num4}`);
+
+function testNum(a) {
+  if (a > 3) {
+    return "Greater Than";
+  } else {
+    return "Less than";
+  }
+}
+
+console.log(testNum(`${num1}`));
+console.log(testNum(`${num2}`));
+console.log(testNum(`${num3}`));
+console.log(testNum(`${num4}`));
